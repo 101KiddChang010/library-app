22ce7d2002d411bfa4b5dc05136bfbe6f9c4f8bb
Kidd <kiddochang02@gmail.com>
2022-09-26 23:29:55 -0400

    Created createCard function to add new books to html

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+//  DOM selectors
+const topContainer = document.getElementById("container-top");
+
+// variables
 let myLibrary = [];
 
+// Functions
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -15,11 +20,41 @@ function addBookToLibrary(userBook) {
 }
 
 function displayLibrary(library) {
+
     for (const books of library) {
         // console.log(books.title);
-        
     }
 }
+
+function createCard(title, author, pages, read) {
+    const newCard = document.createElement("div");
+    const titleParagraph = document.createElement("p");
+    const authorParagraph = document.createElement("p");
+    const pagesParagraph = document.createElement("p");
+    const readParagraph = document.createElement("p");
+    const input = document.createElement("input");
+    newCard.className = "card";
+    input.className = "checkbox";
+    input.setAttribute("type", "checkbox");
+    
+    if (read == true) {
+        input.checked = true;
+    } else {
+        input.checked = false;
+    }
+
+    titleParagraph.textContent = "Title: " + title;
+    authorParagraph.textContent = "Author: " + author;
+    pagesParagraph.textContent = "Number of Pages: " + pages;
+    readParagraph.textContent = "Mark as Read";
+
+    newCard.appendChild(titleParagraph);
+    newCard.appendChild(authorParagraph);
+    newCard.appendChild(pagesParagraph);
+    newCard.appendChild(readParagraph);
+    newCard.appendChild(input);
+}
+createCard("1", "2", "3", true);
 
 let bookOne = new Book("habits", "j.k rowlin", "52", "unread");
 let bookTwo = new Book("money", "rob keyes", "152", "unread");
